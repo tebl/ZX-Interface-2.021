d9825d68bd8e06f1a4486088a938ecc949b9c482
Tor-Eirik Bakke Lunde <teblunde@gmail.com>
2021-05-04 00:01:23 +0200

    Generate last stand routines on the fly

--- software/snapshot_loader/snapshot.asm
+++ software/snapshot_loader/snapshot.asm
@@ -54,6 +54,15 @@ RST_66:	DI
 		RETN					; We won't get here.
 
 ;
+; Macro definitions, this one is mainly used to keep the line count manageable
+; when creating the last stand routine in memory.
+;
+W_BYTE:	.MA
+		LD		(HL),A			; Store A in address at HL, then
+		INC		HL				;  target position.
+		.EM
+
+;
 ; Main program, relocated here to stay clear of the ROM space associated with
 ; the various interrupts. The code will run only once; copying the boot screen
 ; into the screen memory, copy the payload program ("selector program") from
@@ -63,7 +72,9 @@ RST_66:	DI
 ;
 		.NO		$0100,$FF
 MAIN:	DI						; Disable interrupts (just to be safe)
-		; IM		0				; Set interrupt mode 1
+		IM		1				; Set interrupt mode 1
+		LD		HL,(H_SP)		; Place our stack below program stack,
+		LD		SP,HL			;  then configure it for use.
 
 		LD		A,$00			; Load black colour into A
 		OUT		(CTRL_BORDER),A	;  and write to the border.
@@ -98,23 +109,89 @@ MAIN:	DI						; Disable interrupts (just to be safe)
 		LD		DE,TARGET3		; Set target location
 		LDIR					; Have Z80 copy data until all bytes copied
 		
-		; Copy last stand instructions
-		LD		HL,(H_SP)		; Place our stack below program stack,
-		LD		SP,HL			;  then configure it for use.
-		LD		B,17			; Need 13 bytes, add space for 4 bytes of actual stack
-.DEC_B:	DEC		HL
-		DJNZ	.DEC_B
-		PUSH	HL				; This is the start of last stand routine,
-		LD		B,13			;  we have 13 bytes of instructions that
-		LD		DE,LAST_STAND	;  will be read from address in DE (B for B).
-.NEXT_BYTE:
-		LD		A,(DE)			; Load byte
-		LD		(HL),A			;  and store in memory pointed to by HL
-		INC		DE
-		INC		HL
-		DJNZ	.NEXT_BYTE		; Do this until 13 bytes copied.
+		;
+		; Copy last stand instructions from LOADER_ADR, but if the value is all
+		; zeroes then we'll need to generate it ourselves instead. The
+		; rationale is that the cartridge creator should be able to make more
+		; sensible choices than we can.
+		;
+		LD		HL,(LOADER_ADR)	; Get LOADER_ADR from ROM
+		LD		A,0				; Load with a zero
+		CP		H				; Compare against H register,
+		JR		NZ,.COPY		;  use the address if non-zero.
+		CP		L				; Compare against L register,
+		JR		NZ,.COPY		;  use the address if non-zero.
+		
+		LD		HL,(H_SP)		; Generate address, trying below the stack.
+		LD		B,17			; Need 13 bytes, 4 bytes reserved for stack
+.DEC_B:	DEC		HL				;  so we decrement position
+		DJNZ	.DEC_B			;  until B reaches 0.	
+.COPY:	PUSH	HL				; Push the HL as LAST_STAND address for later.
 
-		; Restore registers
+		;
+		; Generating LAST STAND ROUTINE from scratch
+		;
+		LD		A,$21			; LD HL,(H_HL)
+		>W_BYTE
+		LD		A,(H_HL)
+		>W_BYTE
+		LD		A,(H_HL+1)
+		>W_BYTE
+		
+.BANK_OUT:
+		LD		A,$3E			; LD A,%00100000
+		>W_BYTE
+		LD		A,$20
+		>W_BYTE
+		LD		A,$D3			; OUT(CTRL_BANK),A
+		>W_BYTE
+		LD		A,$1F
+		>W_BYTE
+		LD		A,$F1			; POP AF
+		>W_BYTE
+		
+.SET_INTERRUPT_MODE:
+		LD		A,$ED			; Interrupt mode is set using two bytes, the
+		>W_BYTE					;  first one is always $ED.
+		LD		A,(H_INTM)		; Compare interrupt mode,
+		CP		$00				;  does it match 0?
+		JR		NZ,.CHK_IM1		; No, check IM1 instead.
+		LD		A,$46			; Yes, set IM0
+		>W_BYTE
+		JR		.IM_DONE
+.CHK_IM1:
+		CP		$01				; Matches interrupt mode 1?
+		JR		NZ,.CHK_IM2		; No, check IM2 instead.	
+		LD		A,$56			; Yes, set IM1
+		>W_BYTE
+		JR		.IM_DONE
+.CHK_IM2:
+		LD		A,$5E			; Only one left, so we set IM2
+		>W_BYTE
+.IM_DONE:
+
+.SET_INTERRUPT:
+		LD		A,(H_INT)		; Check if Interrupt bit
+		CP		$00				;  is 0, meaning not enabled.
+		JR		NZ,.INT_EI		; Jump ahead if not.
+		LD		A,$00			; We just do a NOP as not enabled from start.
+		>W_BYTE
+		JR		.INT_DONE
+.INT_EI:
+		LD		A,$FB			; Enable maskable interrupts (EI)	
+		>W_BYTE
+.INT_DONE:
+
+.RETN:	LD		A,$ED			; RETN
+		>W_BYTE
+		LD		A,$45
+		>W_BYTE
+		
+		;
+		; Restore all of the registers that we can deal with at the moment,
+		; we'll at a minimum need to restore HL and AF as part of the last
+		; stand routines above.
+		; 
 		LD		A,(H_I)			; Load interrupt page into A
 		LD		I,A				;  and use it to set the page.
 
@@ -138,10 +215,14 @@ MAIN:	DI						; Disable interrupts (just to be safe)
 		PUSH	HL				;  stack so we push the HL register
 		POP		AF				;  to stack and then pop it back.
 	
-		POP		HL				; Pop last stand location
-		PUSH	AF				; Push AF
-		JP		(HL)
+		POP		HL				; Get back the start of last stand routine
+		PUSH	AF				; Push AF to stack for later.
+		JP		(HL)			; Jump to last stand copy in RAM
 
+;
+; This is for the most part included as a template, meaning that most of the
+; actual instructions will have been generated instead.
+;
 		.NO		$0F00
 LAST_STAND:
 		LD		HL,$0100		; We can't use any of the registers, so this
@@ -149,10 +230,10 @@ LAST_STAND:
 		LD		A,%00100000		; Map out the cartridge ROM to enable ZX Basic
 		OUT		(CTRL_BANK),A
 		POP		AF
-		; IM		0			; IM2 ($ED5e), IM1 ($ED56), IM0 ($ED46)
-		NOP
-		NOP
-		; EI					; $FB Replace with NOP if H_INT is Zero
+		IM		0				; IM0 ($ED46), depending on H_INTM.
+		IM		1				; IM1 ($ED56), depending on H_INTM.
+		IM		2				; IM2 ($ED5e), depending on H_INTM.
+		EI						; EI ($FB), replace with NOP if H_INT is Zero
 		NOP
 		RETN
 
@@ -160,6 +241,9 @@ LAST_STAND:
 ; Snapshot header information to be duplicated into every slot, the values
 ; here are just placeholders without any significance (to be replaced later).
 ;
+		.NO		$0FE3
+LOADER_ADR:
+		.DB		$00, $00
 		.NO		$0FE5
 HEADER:
 H_I:	.DB		$00
